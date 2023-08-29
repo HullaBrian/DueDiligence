3c0393508b1291aedf093812a26d50b4ee4d0c89
hullabrian <beierlejc@gmail.com>
2023-08-28 22:32:50 -0500

    Fixed data normalization

--- dd/data/main.py
+++ dd/data/main.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 
 import pandas
+from loguru import logger
 from pandas import DataFrame
 
 from dd.api.main import get_courses
-from dd.api.model import Course
+from dd.api.model import Course, Assignment
 
 
-def _build_assignments(courses: list[Course]) -> DataFrame:
+def _build_assignments(courses: list[Course]) -> tuple[DataFrame, DataFrame]:
+    exams: list[str] = []
+
     assignment_ids: list[int, ...] = []  # For taking out duplicates
     assignments: list[str, ...] = []
     due_dates: list[datetime] = []
@@ -15,7 +18,26 @@ def _build_assignments(courses: list[Course]) -> DataFrame:
     for course in courses:
         for assignment_group in course.assignment_groups:
             if "test" in assignment_group.name.lower() or "exam" in assignment_group.name.lower():
-                continue
+                for test in assignment_group.assignments:
+                    if True not in [val in test.name.lower() for val in ["assignment",
+                                                                         "practice",
+                                                                         "quiz",
+                                                                         "textbook reading"
+                                                                         ]]:
+                        exams.append(
+                            assignment_group.name + " | " +
+                            course.name + " | " +
+                            test.name + " | " +
+                            str(test.due_date)
+                        )
+                    elif test.id not in assignment_ids:
+                        assignments.append(test.name)
+                        due_dates.append(test.due_date)
+                        classes.append(course.name)
+
+                        assignment_ids.append(test.id)
+                        logger.critical(f"Set formally labled exam '{test.name}' to assignment")
+
             for assignment in assignment_group.assignments:
                 if assignment.id not in assignment_ids:
                     assignments.append(assignment.name)
@@ -25,25 +47,32 @@ def _build_assignments(courses: list[Course]) -> DataFrame:
                     assignment_ids.append(assignment.id)
 
     data = {
+        "!! EXAMS !!": exams
+    }
+    exams_df: DataFrame = DataFrame(data)
+
+    data = {
         "Done?": ["" for i in range(len(assignments))],  # TODO: Add checkboxes somehow...
         "Assignment": assignments,
         "Due Date": due_dates,
         "Class": classes
     }
-    df = DataFrame(data)
-    df['Due Date'] = pandas.to_datetime(df['Due Date'])
-    df = df.sort_values(by="Due Date")
+    assignments_df: DataFrame = DataFrame(data)
+    assignments_df['Due Date'] = pandas.to_datetime(assignments_df['Due Date'])
+    assignments_df = assignments_df.sort_values(by="Due Date")
 
-    return df
+    return assignments_df, exams_df
 
 
 def _build_exams():
     pass
 
 
-def build() -> DataFrame:
-    assignments = _build_assignments(get_courses())
+def build() -> None:
+    assignments, exams = _build_assignments(get_courses())
     print(assignments.to_markdown())
+    print("*" * 20)
+    print(exams.to_markdown())
 
 
 if __name__ == "__main__":
